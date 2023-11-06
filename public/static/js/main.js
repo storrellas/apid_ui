9d94f4bbd5e81cb2c17f39a2315e29d39f6746ee
storrellas <storrellas@gmail.com>
2023-11-06 14:25:10 +0100

    ENH: Starting to work

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,7 +1,146 @@
 
 import '/vendor/jquery/dist/jquery.min.js';
+import 'https://unpkg.com/react@18/umd/react.production.min.js';
+import 'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'
+// import ReactDOM from '/vendor/react-dom/client';
+
+
+const AUTHOR = { ME: 'ME', BOT: 'BOT' } 
+const AppId = () => {
+  const [showHideChat, setShowHideChat ] = React.useState(false)
+  const [showChat, setShowChat ] = React.useState(false)
+  const [loading, setLoading ] = React.useState(false)
+  const [messageList, setMessageList ] = React.useState([])
+  const [ message, setMessage ] = React.useState('')
+
+  const sendMessage = async (message) => {
+    const messageListLocal = [...messageList]
+    messageListLocal.push({author: AUTHOR.ME, name: 'Me', message })
+    setMessageList([...messageListLocal ])
+
+    // Do not launch multiple calls
+    if( loading ) return
+    setLoading(true)
+    const conversation_history = messageListLocal.map( item => item.message)
+    conversation_history.pop()
+    const body = {
+      data: {
+        type: "recommendation",
+        attributes: {
+          product_type: message,
+          conversation_history
+        }
+      }
+    }
+    const base_url = window.base_url?window.base_url:"https://apid.duckdns.org"
+
+    const response = await axios.post(`${base_url}/api/chat`, body)    
+    const json_response = response.data.data.attributes
+    messageListLocal.push({author: AUTHOR.BOT, message: json_response.body, name: json_response.name})
+    
+    // messageListLocal.push({author: AUTHOR.BOT, message: "ISay"})
+    // console.log("messageListLocalAfter ", messageList)
+
+    setMessageList(messageListLocal)
+    setLoading(false)
+  }
+
+
+  const onHideChat = () => {
+    setShowHideChat(true)
+    setShowChat(false)
+    setTimeout( () => {
+      setShowHideChat(false)
+    }, 200)
+  }
+
+  const onKeyDownMessage = (e) => {
+    if( e.key === 'Enter'){
+      setMessage('')            
+      sendMessage(message)
+    }
+  }
+
+  const onClickSendMessage = () => {
+    setMessage('')        
+    sendMessage(message)
+  }
+
+  const getClass = () => {
+
+    if( showChat === true )
+      return 'show-chat d-flex flex-column chat-container'
+
+    if( showChat === false ){
+      if (showHideChat === true){
+        return 'hide-chat d-flex flex-column chat-container'
+      }else{
+        return 'd-none'
+      }
+    }
+  }
+
+
+  return <div style={{ position: 'relative' }}>
+            <section role="button" className={showChat?'d-none':'chat-icon'}
+              onClick={() => setShowChat(true)} style={{ zIndex: "100" }}>
+              <img src="/static/img/logomin.png" alt=""></img>
+            </section>  
+
+            <section className={getClass()} style={{ maxWidth: "90%", maxHeight: "90%", zIndex: "100"}}>
+
+              <div className='d-flex align-items-center'>
+                <div className='flex-grow-1 text-center'>
+                  <img src="/static/img/logo.png" height={40} width={130} alt=''></img>
+                </div>
+                <i class="fa fa-times" aria-hidden="true"></i>
+              </div>
+              <div className='flex-grow-1 mt-3 mb-3 rcv-msg-container' style={{ overflowY: 'auto'}}>
+                
+                {messageList.map( (item,idx) =>
+                <div key={idx} className='mt-3 pe-2'>
+                  <div className={`w-100 ${item.author === AUTHOR.BOT?'text-end':'text-start'}`}>
+                    <b>{item.name} says:</b>
+                  </div>
+                  <div style={{ textAlign:'justify' }} key={idx}>{item.message}</div>
+                </div>)}
+
+              </div>
+              {loading?
+              <div className="d-flex justify-content-end align-items-end">
+                <img src="/static/img/spinner.svg" alt="" style={{ height: '60px'}}></img>
+              </div>
+              :null}
+              <div className='d-flex align-items-center justify-content-center input-msg-container'>
+                <input value={message}
+                  className="input-msg" type="text" 
+                  onChange={(e) => setMessage(e.target.value)} 
+                  onKeyDown={(e) => onKeyDownMessage(e)}/>
+                
+                <div className="d-flex align-items-center input-msg-enter"  role='button' onClick={() => onClickSendMessage()}>
+                  <i class="fa fa-square-caret-right" aria-hidden="true"></i>                  
+                </div>
+
+              </div>
+
+            </section>
+
+        </div>
+}
 
 
 $(document).ready( () => {
   console.log("jquery document is ready")
+
+
+  console.log("Rendering react")
+  var iDiv = document.createElement('div');
+  iDiv.id = 'apid';
+  iDiv.className = 'apid';
+  document.getElementsByTagName('body')[0].appendChild(iDiv);
+
+  
+  const root = ReactDOM.createRoot(document.querySelector('#apid'));
+  root.render(<AppId />);
+
 })
