282263b0fe077bfde14f426f24da3b30286d1031
storrellas <storrellas@gmail.com>
2023-11-06 13:49:21 +0100

    ENH: Adding JQuery

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const babelify = require('babelify');
 
+// See: https://gist.github.com/marceloogeda/5a449caa50462ab2667540a93d34009f
 
 function serve() {
   browserSync.init({
       watch: true,
       open: true,
-      server: "./public",
+      server: {
+        baseDir: "./public",
+        routes: {
+            "/vendor": "./node_modules"
+        }
+      },
       ui: { port: 8081 },
       port: 8080
   });
@@ -17,5 +24,5 @@ function serve() {
 // Static Server + watching scss/html files
 exports.serve = serve;
 
-
+// Default task
 exports.default = serve;
---
+++ public/static/js/main.js
@@ -0,0 +1,7 @@
+
+import '/vendor/jquery/dist/jquery.min.js';
+
+
+$(document).ready( () => {
+  console.log("jquery document is ready")
+})
