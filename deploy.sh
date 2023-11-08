63571014102257fc23246f069bbdfa3e813b1259
storrellas <storrellas@gmail.com>
2023-11-08 09:59:24 +0100

    ENH: Improving deployment

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Upload to server
+echo "------------------"
+echo "Deploying to apid.duckdns.org ..."
+echo "------------------"
+ssh apid -t "cd workspace/apid_ui/ && rm -r *"
+scp -r public/* apid:/home/ubuntu/workspace/apid_ui
+
+
+echo "------------------"
+echo "Deploying to runningwarehouse.duckdns.org ..."
+echo "------------------"
+
+ssh -tt apid "rm -r /home/ubuntu/workspace/RWOz/www.runningwarehouse.com.au/apid"
+ssh -tt apid "mkdir /home/ubuntu/workspace/RWOz/www.runningwarehouse.com.au/apid"
 scp -r public/apid/* apid:/home/ubuntu/workspace/RWOz/www.runningwarehouse.com.au/apid
